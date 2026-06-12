7564c6b9f8a6abbebb1b34704be528f397255bed
Fidel Gomez <84531520+fgm97@users.noreply.github.com>
2026-06-12 02:39:34 -0400

    Update load-calendar.js

--- netlify/functions/load-calendar.js
+++ netlify/functions/load-calendar.js
@@ -1,11 +1,28 @@
 exports.handler = async function () {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      hasBinId: !!process.env.JSONBIN_BIN_ID,
-      hasKey: !!process.env.JSONBIN_KEY,
-      binIdLength: process.env.JSONBIN_BIN_ID?.length || 0,
-      keyLength: process.env.JSONBIN_KEY?.length || 0
-    })
-  };
+  try {
+    const response = await fetch(
+      `https://api.jsonbin.io/v3/b/${process.env.JSONBIN_BIN_ID}/latest`,
+      {
+        headers: {
+          "X-Master-Key": process.env.JSONBIN_KEY,
+          "X-Bin-Meta": "false"
+        }
+      }
+    );
+
+    const text = await response.text();
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "text/plain"
+      },
+      body: `Status: ${response.status}\n\n${text}`
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: err.toString()
+    };
+  }
 };
